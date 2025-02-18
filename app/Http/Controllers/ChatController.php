33d16e2bbe8072b633a6c1db6ac49c0a3f057591
Ionis <geras84@yandex.ru>
2025-02-18 23:15:29 +0500

    Add class ChatController

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class ChatController extends Controller
+{
+    /**
+     * @throws GuzzleException
+     */
+    public function chat(Request $request)
+    {
+        $apiKey = config('services.openai.api_key');
+        $userMessage = $request->input('message');
+
+        if (!$userMessage) {
+            return response()->json(['error' => 'No message provided'], 400);
+        }
+
+        $client = new Client();
+        $response = $client->post('https://api.openai.com/v1/chat/completions', [
+            'headers' => [
+                'Authorization' => "Bearer $apiKey",
+                'Content-Type' => 'application/json',
+            ],
+            'json' => [
+                'model' => 'gpt-4',
+                'messages' => [['role' => 'user', 'content' => $userMessage]],
+            ],
+        ]);
+
+        $data = json_decode($response->getBody(), true);
+        return response()->json(['reply' => $data['choices'][0]['message']['content']]);
+    }
+}
